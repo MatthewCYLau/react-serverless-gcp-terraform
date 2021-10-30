22eaa97fc2f768409ab0161404bf720e8d42f2a5
Matthew Lau <lau.cy.matthew@gmail.com>
2021-10-30 17:48:36 +0100

    feat: leverage TypeScript type for PrivateRoute props

--- src/config/PrivateRoute.tsx
+++ src/config/PrivateRoute.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 
-const PrivateRoute = ({ component: Component, ...rest }) => {
+type PrivateRouteProps = {
+  component: React.FunctionComponent<any>;
+  exact: boolean;
+  path: string;
+};
+
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  component: Component,
+  ...rest
+}) => {
   const { isAuthenticated, loading } = useTypedSelector(
     (state) => state.authState
   );
