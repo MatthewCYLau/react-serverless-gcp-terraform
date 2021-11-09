f2f1f1ad3d811407c7661a22f1e68a07e38f29b3
Matthew Lau <lau.cy.matthew@gmail.com>
2021-11-09 22:30:25 +0000

    chore: removed shell script

--- deploy/functions/create-user/update-function.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-zip -r create-user-function.zip index.js package.json && \
-gcloud functions deploy create-user --trigger-http --runtime=nodejs14 --allow-unauthenticated --region=europe-west2 \
---source=gs://react-serverless-cloud-functions-bucket/create-user
