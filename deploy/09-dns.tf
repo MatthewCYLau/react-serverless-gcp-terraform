1a77d88af949f22c44ed3e64f43a3ab34033edfb
matthew.lau <matthew.lau@credera.co.uk>
2023-12-20 14:24:36 +0000

    feat: www dns record set

--- deploy/09-dns.tf
+++ deploy/09-dns.tf
@@ -3,9 +3,18 @@ data "google_dns_managed_zone" "gcpserverless" {
   name     = "gcpserverless-com"
 }
 
-resource "google_dns_record_set" "website" {
+resource "google_dns_record_set" "domain" {
   provider     = google
   name         = data.google_dns_managed_zone.gcpserverless.dns_name
+  type         = "A"
+  ttl          = 300
+  managed_zone = data.google_dns_managed_zone.gcpserverless.name
+  rrdatas      = [google_compute_global_address.external_ip.address]
+}
+
+resource "google_dns_record_set" "www_domain" {
+  provider     = google
+  name         = "www.gcpserverless.com."
   type         = "A"
   ttl          = 300
   managed_zone = data.google_dns_managed_zone.gcpserverless.name
